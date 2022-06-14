a9c534a92e5ce136b69dc8b41793278a6d6bab72
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-06-14 10:40:14 +0300

    feat: adding check on plugins and ECP

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Loader;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Reflection;
@@ -15,6 +16,7 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        public static Form1 Instance;
         public delegate void Add(PrintedEdition printedEdition);
         public delegate void Edit(PrintedEdition printedEdition);
 
@@ -23,12 +25,98 @@ namespace WinFormsApp1
         
         public int index; 
         public List<PrintedEdition> listPrintedEdtions = new List<PrintedEdition>();
+        private static Dictionary<string, IPlugin> Plugins = new Dictionary<string, IPlugin>();
 
         public PrintedEdition printedEdition = new PrintedEdition();
         public Form1()
         {
+            Instance = this;
+            string pathToPlugins = Path.GetFullPath(@"Plugins");
+
+            foreach (var dll in Directory.GetFiles(pathToPlugins, "*.dll"))
+            {
+                AssemblyLoadContext assemblyLoadContext = new AssemblyLoadContext(dll);
+                Assembly assembly = assemblyLoadContext.LoadFromAssemblyPath(dll);
+                IPlugin plugin = Activator.CreateInstance(assembly.GetTypes()[0]) as IPlugin;
+                if (SignIsCorrect(plugin, dll))
+                {
+                    Plugins.Add(Path.GetFileNameWithoutExtension(dll), plugin);
+                }
+                else MessageBox.Show("Incorrect");
+            }
+            foreach (var key in Plugins.Keys)
+            {
+                Plugins[key].Run();
+            }
             InitializeComponent();
 
+        }
+        private bool SignIsCorrect(IPlugin plugin, string path)
+        {
+            long p = 41, q = 59, d = 133;
+            long r = p * q, fr = (p - 1) * (q - 1);
+            long E = Euclid(d, fr);
+            try
+            {
+                StreamReader reader = new StreamReader(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path) + ".txt");
+                long ECPforCheck = long.Parse(reader.ReadLine());
+                string fileDate = reader.ReadLine();
+                reader.Close();
+                reader = new StreamReader(path);
+                long hashCheck = 100;
+                string[] data = File.ReadAllLines(path);
+                int c = reader.Read();
+                while (c != -1)
+                {
+                    hashCheck = (hashCheck + c) * (hashCheck + c) % (p * q);
+                    c = reader.Read();
+                }
+                //if (i != data.Length - 2) hashCheck = (hashCheck + symbol) * (hashCheck + symbol) % r;
+
+                long hashEncr = powByMod(ECPforCheck, E, r);
+
+                if (hashCheck == hashEncr && fileDate == File.GetCreationTime(path).ToString())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            //return false;
+        }
+        static long Euclid(long a, long b)
+        {
+            long x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+            while (b > 0)
+            {
+                long q = a / b, r = a - q * b, x = x2 - q * x1, y = y2 - q * y1;
+                a = b;
+                b = r;
+                x2 = x1;
+                x1 = x;
+                y2 = y1;
+                y1 = y;
+            }
+            return (y2 > 0) ? y2 : x2;
+        }
+        public static long powByMod(long a, long b, long m)
+        {
+            if (b == 0)
+                return 1;
+            if (b % 2 == 0)
+            {
+                long t = powByMod(a, b / 2, m);
+                return t * t % m;
+            }
+
+            return a * powByMod(a, b - 1, m) % m;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
